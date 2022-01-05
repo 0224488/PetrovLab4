f180df5019e81166b90fa6aff79d8470604ae438
0224488 <0224488@inbox.ru>
2022-01-05 21:24:53 +0300

    Без изменеий...

--- PetrovLab4-2/Program.cs
+++ PetrovLab4-2/Program.cs
@@ -10,6 +10,20 @@ namespace PetrovLab4_2
     {
         static void Main(string[] args)
         {
+            // Цикл для поиска квадратов числа через DO...WHILE
+            int sc = 1;
+            int f = 0;
+            int i = 0;
+            Console.WriteLine("Введите число целое число >0:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Квадрат числа {0} = {1}", i + 1, f += sc);
+                sc += 2;
+                i++;
+            }
+            while (i < n);
+            Console.ReadKey();
         }
     }
 }
--- PetrovLab4/Program.cs
+++ PetrovLab4/Program.cs
@@ -10,6 +10,17 @@ namespace PetrovLab4
     {
         static void Main(string[] args)
         {
+            // Цикл для поиска квадратов числа через FOR
+            int sc = 1;
+            int f = 0;
+            Console.WriteLine("Введите число целое число >0:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Квадрат числа {0} = {1}", i + 1, f += sc);
+                sc += 2;
+            }
+            Console.ReadKey();
         }
     }
 }
